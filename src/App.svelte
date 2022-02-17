4dfc81bcc23badca39aac951bcdc0f23287ba248
Trevor Pierce <yeahtrevorpierce@gmail.com>
2022-02-17 09:09:48 -0500

    Further small changes to displaying links for retrieved topics

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,36 @@
 <script>
+  import { onMount } from 'svelte';
+
+  let baseUrl = 'http://localhost:5018/api';
+  let topics = [];
+
+  onMount(async () => {
+    const res = await fetch(`${baseUrl}/topics`,
+      {
+        headers: {
+          "Accept": "application/json"
+        }
+      });
+    topics = await res.json();
+  });
 </script>
 
-<main>
-</main>
+<h1>Forum Topics</h1>
+<ul>
+  {#each topics as topic}
+    <li>
+      <a
+        href="{baseUrl}/topics/{topic.name}"
+        title="{topic.name}"
+        target="_blank"
+      >
+        {topic.name}
+      </a>
+    </li>
+  {:else}
+    <li>loading topics...</li>
+  {/each}
+</ul>
 
 <style>
 </style>
